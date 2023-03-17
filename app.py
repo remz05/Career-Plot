f004ca873e75cc376c4f61054de6ff038de6add4
Ramya Lather <121453961+remz05@users.noreply.github.com>
2023-03-16 20:15:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -99,13 +99,26 @@ def app():
             except TypeError:
                 st.write(f'Sorry, the job title provided is very specific, request you to type a more general title.')  
         
-       
+#         c1, c2, c3 = st.columns(3)
+#         with c1:
+#             st.info('**Data Analyst: [@AliTslm](https://twitter.com/AliTslm)**', icon="💡")
+#         with c2:
+#             st.info('**GitHub: [@alitaslimi](https://github.com/alitaslimi)**', icon="💻")
+#         with c3:
+#             st.info('**Data: [Flipside Crypto](https://flipsidecrypto.xyz)**', icon="🧠")
+        if active_tab != "Career Trajectory":
+               st.sidebar.markdown("[Career Trajectory](#career)")
+        if active_tab != "Machine Learning behind Career Plot":
+               st.sidebar.markdown("[Machine Learning behind Career Plot](#ml)")
+        if active_tab != "Know more about Career Plot":
+               st.sidebar.markdown("[Know more about Career Plot](#more)")
         #st.write(f'Ultimately, our goal is to help job seekers and employees make informed decisions about their careers and maximize their earning potential. We believe that by providing accurate and reliable salary information, we can help to level the playing field and promote greater transparency in the job market.')
         #st.write(f'')      
             
            
         
     if active_tab == "Career Trajectory":
+        st.header("Career Trajectory", id="career")
         st.write(f'**:gray[Curious about career trajectories of professionals with the same profile as you? Tell us more.]**')
         st.write(f'')
         st.write(f'The career graph changes as you type in more information about a profile. It is highly recommended to fill **:blue[Industry]** and **:blue[UG Specialization]** fields')
@@ -149,6 +162,7 @@ def app():
                 st.write(f'Fill in all details')
       
     if active_tab == "Machine Learning behind Career Plot":
+        st.header("Machine Learning behind Career Plot", id="ml")
         #st.write("Below is the architecture behind Career Plot. Consider the job title text - “Associate ” entered by the user. Two set of features are extracted from this text. First, Google word to vec is used to derive 300 dimensional vector representation that captures semantic meaning of the job title text. Second, Page Rank on observed job switches is used to assign relative importance ranks to 1000 most common jobs. Then we use k nearest neighbour to extrapolate rank to all jobs.")
         st.subheader("Methodology")
         st.write(f'Career plot is drawn on profile data collected from LinkedIn and salary data collected from Glassdoor. The data consists of professionals across various industries and educational backgrounds, Masters in Business Administration being the commonality for all.') 
@@ -175,6 +189,7 @@ def app():
         
      
     if active_tab == "Know more about Career Plot":
+        st.header("Know more about Career Plot", id="more")
         st.subheader("Introduction")
         st.write ("In today's job market, individuals need to make informed career decisions to achieve their career goals. Career Plot is a machine learning tool that predicts salaries and builds career trajectories based on job title semantics and order information. The tool is designed to help individuals make informed career decisions by providing insights into salary growth opportunities and career advancement paths.")
         st.write('Career Plot is a valuable tool for individuals and organizations looking to make informed career decisions. With the ability to predict salaries for over 100,000 unique job titles, Career Plot can be a powerful resource for job seekers and employers alike.')
