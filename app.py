d02258fb03890099b34c88388caa1e79e336182f
Ramya Lather <121453961+remz05@users.noreply.github.com>
2024-01-14 13:42:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -153,14 +153,12 @@ def app():
         st.write(f'Career plot is drawn on profile data collected from LinkedIn and salary data collected from Glassdoor. The data consists of professionals across various industries and educational backgrounds, Masters in Business Administration being the commonality for all.') 
         st.write('Its methodology consists of three steps: feature extraction, random forest model training, and career trajectory analysis. The feature extraction step involves extracting semantic and order information from the job title text. The semantic information is captured using Google Word2Vec, which maps the job title text into a 300-dimensional vector space that captures the meaning of the text. The order information is captured using PageRank on observed job switches, which assigns relative importance ranks to 1000 most common jobs.Then KNN (K nearest neighbor) model is used to extrapolate rank to all jobs.') 
         #st.write(f'')
-        image = Image.open('images/job_rank.png')
-        st.image(image)
+        #image = Image.open('images/job_rank.png')
+        #st.image(image)
         #st.write(f'')
         image = Image.open('images/Word_plot.png')
         st.image(image)
         #st.write(f'')
-        
-        
         st.write(f"The next step involves training a random forest model using the feature matrix that combines semantic and order information. The model predicts the salary for the user's job role by leveraging a database of over 100,000 unique job titles.")
         st.write(f"Finally, Career Plot builds a career trajectory based on the user's profile. This trajectory analysis can provide insights into career advancement opportunities and potential salary growth. ")
         #st.write(f'')
