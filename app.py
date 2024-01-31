16583563088659c3aeb418988190f4ff80ca55ca
Ramya Lather <121453961+remz05@users.noreply.github.com>
2024-01-30 20:51:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -146,56 +146,56 @@ def app():
            
         
     if active_tab == "Career Trajectory":
-       user_input = {'ug_degree': 'arts', 'mba_school':'harvard business school', 'industry_category': 'Type your response', 'experience': 1 }
+       # user_input = {'ug_degree': 'arts', 'mba_school':'harvard business school', 'industry_category': 'Type your response', 'experience': 1 }
 
-       if user_input:
-           try:
-               plot_peer_salary_path(df_graph, user_input)
-           except Exception as e:  # Catching any exception
-                st.write(f'An error occurred: {e}')
+       # if user_input:
+       #     try:
+       #         plot_peer_salary_path(df_graph, user_input)
+       #     except Exception as e:  # Catching any exception
+       #          st.write(f'An error occurred: {e}')
        
         
         
-        # st.write(f'**:gray[Curious about career trajectories of professionals with the same profile as you? Tell us more.]**')
-        # #st.write(f'')
-        # st.write(f'The career graph changes as you type in more information about a profile. It is highly recommended to fill **:blue[Industry]** and **:blue[UG Specialization]** fields')
-        # industry_category = st.selectbox('Industry',( 'Select one', 'Computers', 'Entertainment', 'Retail', 'Finance', 'Management',
-        #        'Engineering', 'Science', 'Healthcare', 'Legal', 'Electronics',
-        #        'Art', 'Operations', 'Military', 'Analytics', 'Other'))
-        # #st.write(f'')
-        # st.write(f'Undergraduate Education')
-        # col1, col2, col3 = st.columns(3)
-        # with col1:
-        #     ug_school = st.text_input('UG School', placeholder= 'Type your response')
+        st.write(f'**:gray[Curious about career trajectories of professionals with the same profile as you? Tell us more.]**')
+        #st.write(f'')
+        st.write(f'The career graph changes as you type in more information about a profile. It is highly recommended to fill **:blue[Industry]** and **:blue[UG Specialization]** fields')
+        industry_category = st.selectbox('Industry',( 'Select one', 'Computers', 'Entertainment', 'Retail', 'Finance', 'Management',
+               'Engineering', 'Science', 'Healthcare', 'Legal', 'Electronics',
+               'Art', 'Operations', 'Military', 'Analytics', 'Other'))
+        #st.write(f'')
+        st.write(f'Undergraduate Education')
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            ug_school = st.text_input('UG School', placeholder= 'Type your response')
          
-        # with col2:
-        #     ug_degree = st.selectbox('Specialisation',('Select one','Science', 'Arts', 'Business', 'Other'))
+        with col2:
+            ug_degree = st.selectbox('Specialisation',('Select one','Science', 'Arts', 'Business', 'Other'))
         
-        # with col3:
-        #     ug_grad_year = st.text_input('Completion Year  (Format: YYYY)', key='grad_year_input', placeholder= 'Type your response')
-        # #st.write(f'') 
-        # st.write(f'Postgraduate Education')
-        # col1, col2 = st.columns([2,1])   
-        # with col1:
-        #     mba_school = st.text_input('PG School  (eg: Harvard Business School)', placeholder= 'Type your response')
-        # with col2:
-        #     grad_year = st.text_input('Completion Year (Format: YYYY)', key='post_grad_year_input', placeholder= 'Type your response')
+        with col3:
+            ug_grad_year = st.text_input('Completion Year  (Format: YYYY)', key='grad_year_input', placeholder= 'Type your response')
+        #st.write(f'') 
+        st.write(f'Postgraduate Education')
+        col1, col2 = st.columns([2,1])   
+        with col1:
+            mba_school = st.text_input('PG School  (eg: Harvard Business School)', placeholder= 'Type your response')
+        with col2:
+            grad_year = st.text_input('Completion Year (Format: YYYY)', key='post_grad_year_input', placeholder= 'Type your response')
             
             
             
-        # #st.write(f'')
-        # experience = st.text_input('Years of work experience since your post-graduation  (Format: Integer)', placeholder= 'Type your response')
-        # #st.write(f'')
-        # gender = st.selectbox('Gender',('Select one', 'Male', 'Female', 'Non-Binary'))
+        #st.write(f'')
+        experience = st.text_input('Years of work experience since your post-graduation  (Format: Integer)', placeholder= 'Type your response')
+        #st.write(f'')
+        gender = st.selectbox('Gender',('Select one', 'Male', 'Female', 'Non-Binary'))
 
         
-        # try:
-        #     ug_grad_year_int = int(ug_grad_year) if ug_grad_year else 0
-        #     grad_year_int = int(grad_year) if grad_year else 0
-        #     experience_int = int(experience) if experience else 0
-        # except ValueError:
-        #     st.error("Please enter valid integer values for graduation years and experience.")
-        #     return  # Stop further execution
+        try:
+            ug_grad_year_int = int(ug_grad_year) if ug_grad_year else 0
+            grad_year_int = int(grad_year) if grad_year else 0
+            experience_int = int(experience) if experience else 0
+        except ValueError:
+            st.error("Please enter valid integer values for graduation years and experience.")
+            return  # Stop further execution
             
        
 
